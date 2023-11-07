1be1e7797764dd7309cbd90ece3842206c43ac45
Yasi1095 <yasi1095@stud.kea.dk>
2023-11-07 13:48:03 +0100

    mac solutuion

--- src/main/java/com/example/aibackend/ImageToTextConverter.java
+++ src/main/java/com/example/aibackend/ImageToTextConverter.java
@@ -18,20 +18,23 @@ import java.io.IOException;
 public class ImageToTextConverter implements ApplicationRunner {
 
     public static void main(String [] args) {
+
+        System.setProperty("jna.library.path", "/usr/local/lib"); // Replace with the actual path to your Tesseract library
+
         // Initialize Tesseract
         ITesseract tesseract = new Tesseract();
 
 // Set Tesseract data path
-        tesseract.setDatapath("C:\\Users\\Public\\Documents\\AIApiBackendCloned\\Tess4J\\tessdata");
+        tesseract.setDatapath("/Users/mac1/IdeaProjects/AIApiBackend/Tess4J/tessdata");
 
 // Specify the language (e.g., English)
 // Set image DPI (dots per inch) to improve resolution
         tesseract.setTessVariable("user_defined_dpi", "1000"); // Adjust the DPI as needed
 
-        String folder = "C:\\Users\\Public\\Documents\\AIApiBackendCloned\\src\\main\\java\\com\\example\\aibackend\\";
+       // String folder = "C:\\Users\\Public\\Documents\\AIApiBackendCloned\\src\\main\\java\\com\\example\\aibackend\\";
 
 // Specify the absolute image file path
-        String imagePath = folder + "georgebernardshaw1.jpg"; // Replace with the actual image file path
+        String imagePath = "/Users/mac1/IdeaProjects/AIApiBackend/src/main/java/com/example/aibackend/georgebernardshaw1.jpg"; // Replace with the actual image file path
 
         try {
             // Load and display the image
